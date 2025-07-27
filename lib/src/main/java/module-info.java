f009c55ce4bf6674956d2f3a63a51995c40a62d9
palexdev <alessandro.parisi406@gmail.com>
2025-07-27 14:13:11 +0200

    :recycle: Add requires and exports to module-info.java

--- lib/src/main/java/module-info.java
+++ lib/src/main/java/module-info.java
@@ -1,3 +1,15 @@
 module directory.watcher {
+    /* Dependencies */
+    requires com.sun.jna;
+    requires jdk.unsupported;
+    requires org.tinylog.api;
 
+    /* Exports */
+    exports io.github.palexdev.watcher;
+    exports io.github.palexdev.watcher.changeset;
+    exports io.github.palexdev.watcher.hashing;
+    exports io.github.palexdev.watcher.visitor;
+
+    exports io.github.palexdev.watchservice;
+    exports io.github.palexdev.watchservice.jna;
 }
